42e2093b66d7394ea9f99d5a3f2233108f3b1fbc
BackSeungBeom <seungbeom1234@gmail.com>
2026-05-25 14:50:06 +0900

    feat: 서버 실행시 웹 브라우저 자동 실행

--- src/main/java/com/back/LibraryApplication.java
+++ src/main/java/com/back/LibraryApplication.java
@@ -2,12 +2,23 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class LibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void openBrowser() {
+        try {
+            String url = "http://localhost:8080/account/loginUI";
+            Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", url});
+        } catch (Exception e) {
+            System.err.println("브라우저 자동 실행 실패: " + e.getMessage());
+        }
+    }
 }
