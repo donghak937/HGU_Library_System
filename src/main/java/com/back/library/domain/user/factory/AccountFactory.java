d4d787301e077f0fc567777fbc054f4ab9b96a85
ohjaehyun0827 <phantom030827@gmail.com>
2026-05-26 16:54:07 +0900

    Factory Method Pattern  수정

--- src/main/java/com/back/library/domain/user/factory/AccountFactory.java
+++ src/main/java/com/back/library/domain/user/factory/AccountFactory.java
@@ -2,93 +2,12 @@ package com.back.library.domain.user.factory;
 
 import com.back.library.domain.user.entity.Account;
 
-public class AccountFactory {
+public abstract class AccountFactory {
 
-    public static Account createCitizenAccount(
+    // Factory Method
+    public abstract Account createAccount(
             String accountId,
             String username,
             String passwordHash
-    ) {
-
-        return new Account(
-
-                accountId,
-
-                username,
-
-                passwordHash,
-
-                null,
-
-                "ACTIVE",
-
-                "CITIZEN"
-        );
-    }
-
-    public static Account createStudentAccount(
-            String accountId,
-            String username,
-            String passwordHash
-    ) {
-
-        return new Account(
-
-                accountId,
-
-                username,
-
-                passwordHash,
-
-                null,
-
-                "ACTIVE",
-
-                "STUDENT"
-        );
-    }
-
-    public static Account createAdminAccount(
-            String accountId,
-            String username,
-            String passwordHash
-    ) {
-
-        return new Account(
-
-                accountId,
-
-                username,
-
-                passwordHash,
-
-                null,
-
-                "ACTIVE",
-
-                "ADMIN"
-        );
-    }
-
-    public static Account createLibrarianAccount(
-            String accountId,
-            String username,
-            String passwordHash
-    ) {
-
-        return new Account(
-
-                accountId,
-
-                username,
-
-                passwordHash,
-
-                null,
-
-                "ACTIVE",
-
-                "LIBRARIAN"
-        );
-    }
+    );
 }
---
+++ src/main/java/com/back/library/domain/user/factory/CitizenAccountFactory.java
@@ -0,0 +1,30 @@
+package com.back.library.domain.user.factory;
+
+import com.back.library.domain.user.entity.Account;
+
+public class CitizenAccountFactory
+        extends AccountFactory {
+
+    @Override
+    public Account createAccount(
+            String accountId,
+            String username,
+            String passwordHash
+    ) {
+
+        return new Account(
+
+                accountId,
+
+                username,
+
+                passwordHash,
+
+                null,
+
+                "ACTIVE",
+
+                "CITIZEN"
+        );
+    }
+}
